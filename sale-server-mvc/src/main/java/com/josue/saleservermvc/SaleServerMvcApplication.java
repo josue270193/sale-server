e386173730c41a480dd62277da4ee3ff040f5e81
Josue Lopez <josue270193@gmail.com>
2024-06-11 16:54:48 -0300

    Initial commit

---
+++ sale-server-mvc/src/main/java/com/josue/saleservermvc/SaleServerMvcApplication.java
@@ -0,0 +1,13 @@
+package com.josue.saleservermvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleServerMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleServerMvcApplication.class, args);
+    }
+
+}
---
+++ sale-server-reactive/src/test/java/com/josue/saleserverreactive/SaleServerReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josue.saleserverreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServerReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
