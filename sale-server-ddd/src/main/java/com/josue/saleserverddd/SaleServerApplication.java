cb18a378969b6756d798d8205f821c3e1fec4f4b
Josue Lopez <josue270193@gmail.com>
2024-06-11 16:52:48 -0300

    Initial commit

---
+++ sale-server-ddd/src/main/java/com/josue/saleserverddd/SaleServerApplication.java
@@ -0,0 +1,13 @@
+package com.josue.saleserverddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleServerApplication.class, args);
+    }
+
+}
---
+++ sale-server-ddd/src/test/java/com/josue/saleserver/SaleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josue.saleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
